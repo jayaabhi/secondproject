a059306d0a6c7b9b2b868a4191ab4e3b18b387bb
jayaabhi <jayalakshmi.a1996@gmail.com>
2022-09-02 13:10:15 +0530

    firstcommit

---
+++ src/test/java/NewPro.java
@@ -0,0 +1,17 @@
+import org.testng.annotations.Test;
+
+
+public class NewPro {
+	@Test
+	public void test1() {
+		System.out.println("test1");
+		
+	}
+	@Test
+	public void test2() {
+		System.out.println("test2");
+	}
+
+		
+
+}
